88750c70a618a2ef16987491ad54b4da51c62bf2
premganz <misterprem@gmail.com>
2020-09-21 12:19:23 +0530

    1 file modified

--- src/app/table.component.ts
+++ src/app/table.component.ts
@@ -1,20 +1,31 @@
 import { Component, NgModule,  OnInit } from '@angular/core';
-
+import { EmbedVideoService } from 'ngx-embed-video';
 @Component({
 selector: 'basic-table',
 templateUrl: './basic-table.component.html',
 
 })
-
-
+ 
 
 export class BasicTableComponent {
+ 
+  
+  URLS=[
+    "https://vimeo.com/197933516"
+  ]
   elements: any = [
-    {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
+    {id: 1, first: this.embedService(this.URLS[0]), last: 'Otto', handle: '@mdo'},
     {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
     {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
   ];
 
   headElements = ['ID', 'First', 'Last', 'Handle'];
-
+ constructor(
+    private embedService: EmbedVideoService
+  ){}
 }
+
+ function getEmbedded(url){
+      return this.embedService.embed(url);
+   
+ }
