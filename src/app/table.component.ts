445a3d167a3adf9aed06818ff792d20b0c90e6d2
premganz <misterprem@gmail.com>
2020-09-22 16:31:21 +0530

    3 files modified

--- src/app/table.component.ts
+++ src/app/table.component.ts
@@ -5,19 +5,29 @@ selector: 'basic-table',
 templateUrl: './basic-table.component.html',
 
 })
- 
+
 
 export class BasicTableComponent {
  
   
   URLS=[
-    "https://vimeo.com/197933516"
+    "https://www.youtube.com/Exsljuc_hvs","https://vimeo.com/197933516",
+    "https://youtu.be/ZvUBGN4KoP0",
+    "https://www.youtube.com/watch?v=W4Ou96H-cPc"
   ]
+  // string1=this.embedService(this.URLS[0])
+  string2=''
+  func1: any = function getEmbedded(url){
+      return this.embedService.embed(url);
+   
+  }
+  elements1:any= function getListFromUrls(){
+  
+  }
   elements: any = [
-    {id: 1, first: this.embedService(this.URLS[0]), last: 'Otto', handle: '@mdo'},
+    {id: 1, first:'', last: this.func1(this.URLS[2]), handle: '@mdo'},
     {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
-    {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
-  ];
+     ];
 
   headElements = ['ID', 'First', 'Last', 'Handle'];
  constructor(
@@ -25,7 +35,4 @@ export class BasicTableComponent {
   ){}
 }
 
- function getEmbedded(url){
-      return this.embedService.embed(url);
-   
- }
+ 
